a5b0a43f613dd870d8fe67ff529f02427e224188
rokserak <rok.serak@gmail.com>
2019-02-09 20:58:27 +0100

    lets trade only when volume is 2/3 of average

--- appTestnet.py
+++ appTestnet.py
@@ -48,7 +48,8 @@ while (True):  # work till ban :(
                                      api_key=apiKljuci['websocket']['key'], api_secret=apiKljuci['websocket']['secret'])
                 lastRenew = datetime.now()
 
-            if (ws.get_instrument()['volume'] < ws.get_instrument()['volume24h'] / 24):  # avoid pumps/dumps
+            if (ws.get_instrument()['volume'] < ws.get_instrument()['volume24h'] / 24 / 0.7):  # avoid pumps/dumps
+                # only trade when volume is 2/3 of average volume
 
                 d = dateutil.parser.parse(ws.get_instrument()['fundingTimestamp'])
                 d = d.replace(tzinfo=None)
